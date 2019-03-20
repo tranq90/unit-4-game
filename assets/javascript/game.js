822a723c4ab99778e2c3033638e127fcd98da7b1
quan <tran.q90@gmail.com>
2019-03-19 18:01:51 -0700

    logic stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,47 @@
 $(document).ready(function () {
 
-    var darthSidious = {
-        id: "sidious",
-        name: "Darth Sidious",
-        health: 100,
-        alive: true
-    }
+    var characters = [
+        {
+            id: "1",
+            name: "Darth Maul",
+            health: 100,
+            alive: true
+        },
+        {
+            id: "2",
+            name: "Darth Sidious",
+            health: 100,
+            alive: true
+        },
+        {
+            id: "3",
+            name: "Obi-Wan-Kenobi",
+            health: 100,
+            alive: true
+        },
+        {
+            id: "4",
+            name: "Luke Skywalker",
+            health: 100,
+            alive: true
+        }
 
-    var darthMaul = {
-        id: "maul",
-        name: "Darth Maul",
-        health: 100,
-        alive: true
-    }
+    ]
 
-    var obiWanKenobi = {
-        id: "kenobi",
-        name: "Obi-Wan-Kenobi",
-        health: 100,
-        alive: true
-    }
-
-    var lukeSkywalker = {
-        id: "skywalker",
-        name: "Luke Skywalker",
-        health: 100,
-        alive: true
-    }
-
-    function attack(attacker, defender) {
+    function attack(attackerID, defenderID) {
         var attackerAP = Math.floor(Math.random() * 100);
         var defenderAP = Math.floor(Math.random() * 100);
-        attacker.health -= defenderAP;
-        if (attacker.health <= 0) {
+        // if (attacker.health <= 0) {
 
-        }
-        defender.health -= attackerAP;
-        if (defender.health <= 0) {
+        // }
+        // if (defender.health <= 0) {
 
-        }
+        // }
+
+        Object.keys(lukeSkywalker).forEach(function(key){
+            console.log(key, lukeSkywalker[key]);
+        })
+        
         $("#attacking").html("You hit " + defender.name + " for " + attackerAP + " damage");
     }
 
@@ -46,8 +49,30 @@ $(document).ready(function () {
     var yourEnemy = "";
     $("img").click(function (event) {
         if (yourCharacter == "") {
-            $("#your-character").append("<img src='assets/images/" + event.target.id + ".jpg' />");
+            $("#your-character").append("<img id='" + event.target.id + "' src='assets/images/" + event.target.id + ".jpg' />");
+            $("#characters").html("");
             yourCharacter = event.target.id;
+
+            for (var i = 1; i <= 4; i++) {
+                if (i != event.target.id) {
+                    $("#enemies").append("<img id='" + i + "' src='assets/images/" + i + ".jpg' />");
+                }
+            }
+
         }
+        $("img").click(function (event) {
+            if (yourEnemy == "") {
+                $("#defender").append("<img id='" + event.target.id + "' src='assets/images/" + event.target.id + ".jpg' />");
+                yourEnemy = event.target.id;
+            }
+        })
+
+    })
+
+
+    $("#attack").click(function () {
+        var attackerID = $("#your-character > img").attr("id");
+        var defenderID = $("#defender > img").attr("id");
+        attack(attackerID, defenderID);
     })
 })
